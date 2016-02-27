84f4a1c6915ea314f99777de8ed4186344a33cdb
tylerwiersing <tyler.wiersing@gmail.com>
2016-02-27 05:54:15 -0500

    adding stupid button thing, it kinda works

---
+++ Assets/Scripts/scrollViewButtonTemplate.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class scrollViewButtonTemplate : MonoBehaviour
+{
+
+    private string Name;
+    public Text ButtonText;
+    public scrollViewTemplate ScrollView;
+
+    public void SetName(string name)
+    {
+        Name = name;
+        ButtonText.text = name;
+    }
+    public void Button_Click()
+    {
+        ScrollView.ButtonClicked(Name);
+
+    }
+}
---
+++ Assets/Scripts/scrollViewTemplate.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class scrollViewTemplate : MonoBehaviour
+{
+
+    public GameObject Button_Template;
+    private List<string> NameList = new List<string>();
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        NameList.Add("Alan");
+        NameList.Add("Amy");
+        NameList.Add("Brian");
+        NameList.Add("Carrie");
+        NameList.Add("David");
+        NameList.Add("Joe");
+        NameList.Add("Jason");
+        NameList.Add("Michelle");
+        NameList.Add("Stephanie");
+        NameList.Add("Zoe");
+
+        foreach (string str in NameList)
+        {
+            GameObject go = Instantiate(Button_Template) as GameObject;
+            Debug.Log(str);
+            Debug.Log(go);
+            go.SetActive(true);
+            scrollViewButtonTemplate TB = go.GetComponent<scrollViewButtonTemplate>();
+            TB.SetName(str);
+            go.transform.SetParent(Button_Template.transform.parent);
+
+        }
+
+
+    }
+
+    public void ButtonClicked(string str)
+    {
+        Debug.Log(str + " button clicked.");
+
+    }
+}
