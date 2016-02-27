4398375802e4df34d3427cd952bb258c3f692226
Kamil Jarosz <kjarosz19946@gmail.com>
2016-02-26 23:43:27 -0500

    Movable paddle.

---
+++ Assets/Scripts/PaddleMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleMove : MonoBehaviour {
+    public float paddleSpeed;
+    public float minX;
+    public float maxX;
+
+    private Rigidbody2D body;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+	
+	void FixedUpdate () 
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        Vector2 vel = body.velocity;
+        vel.x = horizontal * paddleSpeed;
+        body.velocity = vel;
+
+        Vector2 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        transform.position = pos;
+	}
+}
