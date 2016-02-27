6ee343f279b6da03382ac447f064f85490976796
Kamil Jarosz <kjarosz19946@gmail.com>
2016-02-27 09:47:00 -0500

    Add breakable bricks.

---
+++ Assets/Scripts/DestroyBlock.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyBlock : MonoBehaviour {
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.CompareTag("Ball"))
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
