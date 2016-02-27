eda99fb314670296fa16794e4fa0f81879c7876f
zonte94 <mborsos94@gmail.com>
2016-02-27 12:24:39 -0500

    Added sounds and particle effect's to Kamil's scene

--- Assets/Scripts/DestroyBlock.cs
+++ Assets/Scripts/DestroyBlock.cs
@@ -2,11 +2,19 @@
 using System.Collections;
 
 public class DestroyBlock : MonoBehaviour {
+        public GameObject[] brickParticles;
+	// Use this for initialization
+	void Start () {
+	
+	}
 
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.collider.CompareTag("Ball"))
         {
+            int particle;
+            particle = UnityEngine.Random.Range(0, brickParticles.Length);
+            Instantiate(brickParticles[particle], transform.position, Quaternion.identity);
             GameObject.Destroy(gameObject);
         }
     }
