acbfb643f33b94c70c8f22a1e1458e9fa4b2058c
Kamil Jarosz <kjarosz19946@gmail.com>
2016-02-27 13:21:44 -0500

    Restrict spawn angles.

--- Assets/Scripts/LaunchBall.cs
+++ Assets/Scripts/LaunchBall.cs
@@ -24,17 +24,10 @@ public class LaunchBall : MonoBehaviour {
         if (Input.GetButtonDown("Activate"))
         {
             // Pick out a random direction within constraints
-            int minAngle = 90 - angleFromVertical;
-            int maxAngle = 90 + angleFromVertical;
-            float angle = Random.Range(minAngle, maxAngle);
-            Vector2 velocity = new Vector2();
-            velocity.x = Mathf.Cos(angle);
-            velocity.y = -Mathf.Sin(angle);
+            float angle = Random.Range(-angleFromVertical, angleFromVertical);
+            Vector2 velocity = Quaternion.Euler(0.0f, 0.0f, angle) * Vector2.up;
             Rigidbody2D body = GetComponent<Rigidbody2D>();
             body.velocity = velocity * startingSpeed;
-
-            Debug.DrawRay(transform.position, transform.position + Quaternion.Euler(0.0f, 0.0f, minAngle) * Vector3.up);
-            Debug.DrawRay(transform.position, transform.position + Quaternion.Euler(0.0f, 0.0f, maxAngle) * Vector3.up);
 
             GameObject.DestroyObject(this);
             source.PlayOneShot(clip);
