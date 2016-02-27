ce46555a594a5c81eaf1735c3ea127e7ff1961c1
tylerwiersing <tyler.wiersing@gmail.com>
2016-02-27 05:06:33 -0500

    basic server/room creation

---
+++ Assets/Scripts/refreshServerButton.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class refreshServerButton : MonoBehaviour {
+
+    public GameObject ButtonTemplate;
+    private bool refreshingHostList = false;
+    private string gameTypeName = "Breakout";
+
+    public void RefreshServerList()
+    {
+        if (!refreshingHostList)
+        {
+            refreshingHostList = true;
+            MasterServer.ClearHostList();
+            MasterServer.RequestHostList(gameTypeName);
+        }
+    }
+
+    private void JoinServer(HostData hostData)
+    {
+        Network.Connect(hostData);
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (MasterServer.PollHostList().Length != 0)
+        {
+            HostData[] hostList = MasterServer.PollHostList();
+            Debug.Log("here");
+            for (int i = 0; i < hostList.Length; i++)
+            {
+                Debug.Log(hostList[i].gameName);
+                GameObject serverScrollViewObject = GameObject.Find("serverScrollView");
+                ScrollRect serverScrollViewRect = serverScrollViewObject.GetComponent<ScrollRect>();
+            }
+            MasterServer.ClearHostList();
+        }
+	}
+    
+}
